3b9816418abd54858ee9141d68f879729fe9183d
Jasmine Mithani <jazzmithani@gmail.com>
2019-01-26 10:54:38 -0600

    correctly processing files

--- format_files.py
+++ format_files.py
@@ -2,11 +2,12 @@ import os
 
 directory = 'lyrics/unique/processed/'
 files = os.listdir(directory)
-
 for filename in files:
     f = open(directory + filename, 'r')
     text = f.read()
 
     lines = [text.lower() for line in filename]
     with open(directory + filename, 'w') as out:
-        out.writelines(lines)
+        out.writelines(lines[0])
+        out.close()
+    f.close()
